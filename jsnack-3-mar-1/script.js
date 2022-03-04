1f981abe38e6cbe5b64df6147d82225911ef9110
Ezy93 <giordani001@gmail.com>
2022-03-04 17:06:45 +0100

    completato jsnack incopleto

--- jsnack-3-mar-1/script.js
+++ jsnack-3-mar-1/script.js
@@ -1,19 +1,25 @@
 /**
  *
  *
-    Crea due tag div con due id diversi:
-    un div avrà il testo colorato di rosso mentre l'altro di verde.
-    Partendo da un array di numeri, creo una funzione che controlla se siano pari o dispari.
-    Se sono pari li stampo nell'div "rosso",
-    se sono dispari li stampo nell'div "verde".
+Crea due tag div con due id diversi:
+un div avrà il testo colorato di rosso mentre l'altro di verde.
+Partendo da un array di numeri, creo una funzione che controlla se siano pari o dispari.
+Se sono pari li stampo nell'div "rosso",
+se sono dispari li stampo nell'div "verde".
  *
  */
 
 
-    let numberArray = [10 ,20, 30,40, 50 , 60, 100];
+   let numberArray = [10 ,23, 30, 47 , 50 , 69 , 104];
 
-    function oddOEven(array){
-        for{
-            
-        }
-    }
+   function oddOEven(array){
+      for(let i = 0; i < array.length ; i++){
+         if(array[i] % 2 == 0){
+            document.getElementById("red").innerHTML += array[i];
+         }else{
+            document.getElementById("green").innerHTML += array[i];
+         }
+      }
+   }
+
+   oddOEven(numberArray);
